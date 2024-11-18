89e4e5b2465d0f52385740cea6e9c6ac1c1a39b8
Brandon Lin <brandonlin9716@gmail.com>
2024-11-17 18:46:46 -0800

    Move output to bottom

--- webpack.common.js
+++ webpack.common.js
@@ -3,11 +3,6 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
@@ -32,5 +27,10 @@ module.exports = {
         type: "asset/resource",
       },
     ],
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
 };
